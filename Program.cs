e23338f26a935459a5184b6d7e4fd47308277f5f
falau <falau@LENOVO>
2024-10-29 20:32:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Set1Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti primul numar: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("introduceti al doilea numar: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Valorile initiale sunt a:{a}, b:{b}");
+            double temp;
+
+            temp = a;
+            a = b; 
+            b = temp;
+            Console.WriteLine($"Valorile dupa swap sunt a:{a}, b:{b}");
+
+        }
+    }
+}
